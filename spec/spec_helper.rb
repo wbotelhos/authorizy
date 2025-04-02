cf15be211aefd8cbd67275d5ec2d7ed9174b5c29
Washington Botelho <wbotelhos@gmail.com>
2025-04-02 10:48:27 -0300

    spec: lint

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,16 @@
 
 def config_mock(aliases: nil, cop: nil, current_user: nil, dependencies: nil, redirect_url: nil)
   backup = {
-    aliases:      Authorizy.config.aliases,
-    cop:          Authorizy.config.cop,
+    aliases: Authorizy.config.aliases,
+    cop: Authorizy.config.cop,
     current_user: Authorizy.config.current_user,
     dependencies: Authorizy.config.dependencies,
     redirect_url: Authorizy.config.redirect_url,
   }
 
   Authorizy.configure do |config|
-    config.aliases      = aliases                        if aliases
-    config.cop          = cop                            if cop
+    config.aliases = aliases if aliases
+    config.cop = cop if cop
     config.current_user = ->(_context) { current_user } if current_user
     config.dependencies = dependencies if dependencies
     config.redirect_url = ->(_context) { redirect_url } if redirect_url
@@ -20,8 +20,8 @@ def config_mock(aliases: nil, cop: nil, current_user: nil, dependencies: nil, re
   yield
 ensure
   Authorizy.configure do |config|
-    config.aliases      = backup[:aliases]
-    config.cop          = backup[:cop]
+    config.aliases = backup[:aliases]
+    config.cop = backup[:cop]
     config.current_user = backup[:current_user]
     config.dependencies = backup[:dependencies]
     config.redirect_url = backup[:redirect_url]
